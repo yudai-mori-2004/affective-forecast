25d813c77dd0e15c1a79dba9fc4f68d4984ed06c
yudai-mori-2004 <forestocean0119@gmail.com>
2025-07-02 13:53:23 +0900

    initial commit

---
+++ 02_データの統計量調査/src/subject.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from util.utils import search_data, plot_wave
+
+if __name__ == "__main__":
+    # 使用例: 特定の被験者のデータを検索して描画
+    data_list = search_data(subID="S01")
+    print(f"検索結果: {len(data_list)}件のデータが見つかりました")
+    
+    # データが存在する最初の項目を描画
+    for i, data in enumerate(data_list):
+        if data["exist_data"]:
+           # print(f"[{i}]番目のデータを描画します")
+            plot_wave(data, "eda", 0, save_at=f"/home/mori/projects/affective-forecast/02_データの統計量調査/plots/S01/eda/row1/plt_{data["date"]}")
+            
+    else:
+        print("描画可能なデータが見つかりませんでした")
